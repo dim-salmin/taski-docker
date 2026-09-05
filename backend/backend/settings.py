955b3c3e7eddcdd362b69ef4d6182fc7a276c7ea
Dmitry Salmin <ds.salmin@gmail.com>
2026-09-05 18:32:14 +0300

    fix flake8

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -69,8 +69,7 @@ DATABASES = {
         'HOST': os.getenv('DB_HOST', ''),
         'PORT': os.getenv('DB_PORT', 5432)
     }
-} 
-
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
